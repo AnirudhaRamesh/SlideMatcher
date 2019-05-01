a794efb3a6735bb643152b862788b1604433c768
Aryan Sakaria <aryan.sakaria@students.iiit.ac.in>
2019-05-01 08:25:23 -0400

    stolen code2

--- mkI.py
+++ mkI.py
@@ -23,9 +23,22 @@
 # plt.imshow(img3),plt.show()
 import numpy as np
 import cv2 as cv
-img = cv.imread('contrast.png')
-gray= cv.cvtColor(img,cv.COLOR_BGR2GRAY)
+import matplotlib.pyplot as plt
+img1 = cv.imread('0.jpg',cv.IMREAD_GRAYSCALE)          # queryImage
+img2 = cv.imread('ppt.jpg',cv.IMREAD_GRAYSCALE) # trainImage
+# Initiate SIFT detector
 sift = cv.xfeatures2d.SIFT_create()
-kp = sift.detect(gray,None)
-img = cv.drawKeypoints(gray,kp,img)
-cv.imwrite('sift_keypoints.jpg',img)
+# find the keypoints and descriptors with SIFT
+kp1, des1 = sift.detectAndCompute(img1,None)
+kp2, des2 = sift.detectAndCompute(img2,None)
+# BFMatcher with default params
+bf = cv.BFMatcher()
+matches = bf.knnMatch(des1,des2,k=2)
+# Apply ratio test
+good = []
+for m,n in matches:
+    if m.distance < 0.75*n.distance:
+        good.append([m])
+# cv.drawMatchesKnn expects list of lists as matches.
+img3 = cv.drawMatchesKnn(img1,kp1,img2,kp2,good,None,flags=cv.DrawMatchesFlags_NOT_DRAW_SINGLE_POINTS)
+plt.imshow(img3),plt.show()
