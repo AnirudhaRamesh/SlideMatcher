e074c51f6c91f6e885ec1ed534c3ccc0a6ca1f55
Anirudha123 <anirudha.ramesh@students.iit.ac.in>
2019-05-01 17:45:56 +0530

    SIFT + KNN working

--- mkI.py
+++ mkI.py
@@ -1,48 +1,47 @@
-# import numpy as numpy
-# import cv2
-# from matplotlib import pyplot as plt
-
-# img1 = cv2.imread('contrast.png', 0)
-# img2 = cv2.imread('contrast-truth.png', 0)
-
-# orb = cv2.ORB_create()
-
-# kp1, des1 = orb.detectAndCompute(img1, None)
-# kp2, des2 = orb.detectAndCompute(img2, None)
-
-# bf = cv2.BFMatcher(cv2.NORM_HAMMING, crossCheck = True)
-
-# matches = bf.match(des1,des2)
-
-# # Sort them in the order of their distance.
-# matches = sorted(matches, key = lambda x:x.distance)
-
-# # Draw first 10 matches.
-# img3 = cv2.drawMatches(img1,kp1,img2,kp2,matches, None, flags=2)
-
-# plt.imshow(img3),plt.show()
 import numpy as np
 import cv2 as cv
 import matplotlib.pyplot as plt
-img1 = cv.imread('4.jpg',cv.IMREAD_GRAYSCALE)          # queryImage
-img2 = cv.imread('ppt.jpg',cv.IMREAD_GRAYSCALE) # trainImage
-# Initiate SIFT detector
+img_array = [] 
+truth_array = []
+truth_array.append(cv.imread('sample_test/slides/ppt1.jpg',cv.IMREAD_GRAYSCALE)) # trainImage
+truth_array.append(cv.imread('sample_test/slides/ppt2.jpg',cv.IMREAD_GRAYSCALE)) 
+
+for i in range(0,10):
+    img_path = 'sample_test/frames/'+str(i)+'.jpg'
+    img_array.append(cv.imread(img_path,cv.IMREAD_GRAYSCALE))      # queryImage
+
+# Initiate SIFT detector and BFMatcher 
 sift = cv.xfeatures2d.SIFT_create()
-# find the keypoints and descriptors with SIFT
-kp1, des1 = sift.detectAndCompute(img1,None)
-kp2, des2 = sift.detectAndCompute(img2,None)
-# BFMatcher with default params
 bf = cv.BFMatcher()
-matches = bf.knnMatch(des1,des2,k=2)
-# Apply ratio test
-good = []
-good_count = 0 
-for m,n in matches:
-    if m.distance < 0.75*n.distance:
-        good.append([m])
-        good_count += 1
+# find the keypoints and descriptors with SIFT
+SIFT_features_array =  []  
+for l in range(0,len(truth_array)):
+    temp_holder, temp_features = sift.detectAndCompute(truth_array[l],None)
+    SIFT_features_array.append(temp_features)
+
+#old code below
+# kp1, des1 = sift.detectAndCompute(img1,None)
+# kp2, des2 = sift.detectAndCompute(img2,None)
+# BFMatcher with default params
+
+for i in range(0,10) :
+    max_good_count = [-1,-1]
+    kp2, des2 = sift.detectAndCompute(img_array[i],None)
+    for l in range(0,len(truth_array)) :
+        temp = SIFT_features_array[l]
+        matches = bf.knnMatch(temp,des2,k=2)
+        # Apply ratio test
+        good = []
+        good_count = 0 
+        for m,n in matches:
+            if m.distance < 0.75*n.distance:
+                good.append([m])
+                good_count += 1
 # cv.drawMatchesKnn expects list of lists as matches.
-img3 = cv.drawMatchesKnn(img1,kp1,img2,kp2,good,None,flags=cv.DrawMatchesFlags_NOT_DRAW_SINGLE_POINTS)
-plt.imshow(img3),plt.show()
-print(good[0])
-print(good_count)
+# img3 = cv.drawMatchesKnn(img1,kp1,img2,kp2,good,None,flags=cv.DrawMatchesFlags_NOT_DRAW_SINGLE_POINTS)
+# plt.imshow(img3),plt.show()
+        if max_good_count[1] < good_count :
+            max_good_count[1] = good_count
+            max_good_count[0] = l
+
+    print("BEST MATCH FOR IMG", i, "IS ", max_good_count)
